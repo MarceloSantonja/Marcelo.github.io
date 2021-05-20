20d8aa4ba98c2d331b935c444fd7eb742438e7f5
Marcelo <Marcelo.Santonja@iesdoctorbalmis.com>
2021-05-20 23:07:33 +0200

    a

--- Ejercicio1/Librerias.js
+++ Ejercicio1/Librerias.js
@@ -8,7 +8,7 @@ function gestionarFicheroXML(xmlDoc){
 	
 	console.log(libreria.length);
 	let libros = libreria[0].querySelectorAll("Libros");
-
+	console.log(libros.lenght);
 	for(let i=0; i<libros.length; i++)
 		capaVacia.innerHTML = capaVacia.innerHTML + "<p>" + libros[i].textContent + "</p>"
 	
