d957d1c210ac251ab14f48ce699091d382349a6c
carmenFRP <33463902+carmenFRP@users.noreply.github.com>
2021-05-20 11:51:38 +0200

    Update leerFicheroXML.js

--- Ejercicio1/leerFicheroXML.js
+++ Ejercicio1/leerFicheroXML.js
@@ -1,9 +1,10 @@
 
 function gestionarFicheroXML(xmlDoc){
-	let capaVacia = document.querySelector("#ficheroXML")
+	alert(xmlDoc)
+	/*let capaVacia = document.querySelector("#ficheroXML")
 	let alumnos = xmlDoc.querySelectorAll("alumno")
 	for(let i=0; i<alumnos.length; i++)
-		capaVacia.innerHTML = capaVacia.innerHTML + "<p>" + alumnos[i].textContent + "</p>"
+		capaVacia.innerHTML = capaVacia.innerHTML + "<p>" + alumnos[i].textContent + "</p>"*/
 	
 }
 
