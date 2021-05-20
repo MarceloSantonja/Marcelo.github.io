26492abb167c2a18f53863c4a616c4310e6006dd
Marcelo <Marcelo.Santonja@iesdoctorbalmis.com>
2021-05-21 00:50:00 +0200

    cambiando comillas intentando que me muestre lo que hay en capaVacia

--- Ejercicio1/Librerias.js
+++ Ejercicio1/Librerias.js
@@ -11,17 +11,17 @@ function gestionarFicheroXML(xmlDoc){
 
 	let libros = xmlDoc.querySelectorAll("libro");
 
-	capaVacia.outerHTML += "<div id = \"Tabla\">";
+	capaVacia.innerHTML += "<div id = \"Tabla\">";
 	for (let i = 1; i <= libreria.length; i++) {
-		capaVacia.outerHTML += "<div id= \"Libreria\">"+nombre[i-1].textContent+"</div><div class=\"fila\"> <div class=\"celda\">ISBN</div> <div class=\"celda\">titulo</div> <div class=\"celda\">Nivel de profundidad</div> <div class=\"celda\">autor</div> <div class=\"celda\">editorial</div> <div class=\"celda\">fecha de publicacion</div> <div class=\"celda\">pagina Web</div ><div class=\"celda\">precio</div></div><div class=\"fila\">";
+		capaVacia.innerHTML += `<div id= "Libreria">`+nombre[i-1].textContent+`</div><div class="fila"> <div class="celda">ISBN</div> <div class="celda">titulo</div> <div class="celda">Nivel de profundidad</div> <div class="celda">autor</div> <div class="celda">editorial</div> <div class="celda">fecha de publicacion</div> <div class="celda">pagina Web</div ><div class="celda">precio</div></div><div class="fila">`;
 
 		for(let j=1; j<=3; j++){
-		capaVacia.outerHTML = capaVacia.outerHTML + "<div class=\"celda\">" + libros[(i*j)-1].textContent + "</div>";
+		capaVacia.innerHTML = capaVacia.innerHTML + `<div class="celda">` + libros[(i*j)-1].textContent + `</div>`;
 		}
-		capaVacia.outerHTML +="</div>";
+		capaVacia.innerHTML +=`</div>`;
 	}
-	capaVacia.outerHTML += "</div>";
-	
+	capaVacia.innerHTML += `</div>`;
+	console.log(capaVacia.nodeValue);
 	
 }
 
