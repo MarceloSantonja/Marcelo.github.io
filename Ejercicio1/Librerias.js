3676fa0138c6fac35e771843c8b8ad33bbcbb09d
Marcelo <Marcelo.Santonja@iesdoctorbalmis.com>
2021-05-21 00:11:20 +0200

    a

--- Ejercicio1/Librerias.js
+++ Ejercicio1/Librerias.js
@@ -6,11 +6,11 @@ function gestionarFicheroXML(xmlDoc){
 	let capaVacia = document.querySelector("#ContenedorLibros");
 	let libreria = xmlDoc.querySelectorAll("libreria");
 	let nombre = xmlDoc.querySelectorAll("nombre");
-	console.log(nombre.textContent);
+	console.log(nombre[0].textContent);
 	
-	console.log(libreria.length);
+
 	let libros = xmlDoc.querySelectorAll("libro");
-	console.log(libros.length);
+
 	capaVacia.innerHTML += "<div id = \"Tabla\">";
 	for (let i = 0; i < libreria.length; i++) {
 		capaVacia.innerHTML += "<div id= \"Libreria\">"+nombre[i].textContent+"</div><div class=\"fila\"> <div class=\"celda\">ISBN</div> <div class=\"celda\">titulo</div> <div class=\"celda\">Nivel de profundidad</div> <div class=\"celda\">autor</div> <div class=\"celda\">editorial</div> <div class=\"celda\">fecha de publicacion</div> <div class=\"celda\">pagina Web</div ><div class=\"celda\">precio</div></div><div class=\"fila\">";
@@ -21,7 +21,7 @@ function gestionarFicheroXML(xmlDoc){
 		capaVacia.innerHTML +="</div>";
 	}
 	capaVacia.innerHTML += "</div>";
-
+	console.log(capaVacia.textContent);
 	
 }
 
