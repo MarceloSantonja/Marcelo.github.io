9a82ab0ee071a5bf73830740ca15693343777e22
Marcelo <Marcelo.Santonja@iesdoctorbalmis.com>
2021-05-21 15:13:09 +0200

    a

--- Ejercicio2/chat.js
+++ Ejercicio2/chat.js
@@ -4,8 +4,10 @@
 function gestionarFicheroTXT(txtDoc){
 	console.log(txtDoc);
 	let capaVacia = document.querySelector("#CanalChat");
-
-
+	txtDoc = " hola";
+	console.log(txtDoc);
+	txtDoc += " adios";
+	console.log(txtDoc);
 	
 }
 
