f447e2f5a82349b0dc1e49876dcf0fc819c6116a
Marcelo <Marcelo.Santonja@iesdoctorbalmis.com>
2021-05-21 16:18:55 +0200

    a

--- Ejercicio2/chat.js
+++ Ejercicio2/chat.js
@@ -4,18 +4,17 @@
 function gestionarFicheroTXT(txtDoc){
 	console.log(txtDoc);
 	let capaVacia = document.querySelector("#CanalChat");
-	capaVacia = "";
 	let chat = txtDoc.split(`\n`);
 	console.log(chat.length);
 	for (let i = 0; i < chat.length; i++) {
 
 		if (i%2 ==0) {
-			capaVacia = capaVacia + `<p style="background-color:wheat; text-align: left;">`+ chat[i]+ `</p>`;
+			capaVacia.innerHTML = capaVacia.innerHTML + `<p style="background-color:wheat; text-align: left;">`+ chat[i]+ `</p>`;
 		}else{
-			capaVacia = capaVacia + `<p style="background-color: aquamarine; text-align: right;">`+ chat[i]+ `</p>`;
+			capaVacia.innerHTML = capaVacia.innerHTML + `<p style="background-color: aquamarine; text-align: right;">`+ chat[i]+ `</p>`;
 		}
 	}
-	console.log(capaVacia.textContent);
+	console.log(capaVacia.innerHTML.textContent);
 	
 }
 
