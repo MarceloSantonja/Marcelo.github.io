b2e00684c53f84434596ffa5e0348eb3e86289dd
Marcelo <Marcelo.Santonja@iesdoctorbalmis.com>
2021-05-21 16:10:29 +0200

    comprobando que hecho mal

--- Ejercicio2/chat.js
+++ Ejercicio2/chat.js
@@ -5,13 +5,16 @@ function gestionarFicheroTXT(txtDoc){
 	console.log(txtDoc);
 	let capaVacia = document.querySelector("#CanalChat");
 	let chat = txtDoc.split(`\n`);
+	console.log(chat.length);
 	for (let i = 0; i < chat.length; i++) {
+
 		if (i%2 ==0) {
 			capaVacia = capaVacia + `<p style="background-color:wheat; text-align: left;">`+ chat[i]+ `</p>`;
 		}else{
 			capaVacia = capaVacia + `<p style="background-color: aquamarine; text-align: right;">`+ chat[i]+ `</p>`;
 		}
 	}
+	console.log(capaVacia.textContent);
 	
 }
 
