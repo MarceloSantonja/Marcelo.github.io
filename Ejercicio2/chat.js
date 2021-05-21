1a36f4b1afe2456bf29a71b52ee9e746a209c7b9
Marcelo <Marcelo.Santonja@iesdoctorbalmis.com>
2021-05-21 14:31:18 +0200

    intentando txt

---
+++ Ejercicio2/chat.js
@@ -0,0 +1,16 @@
+'use strict';
+
+
+function gestionarFicheroXML(txtDoc){
+	alert(txtDoc);
+	let capaVacia = document.querySelector("#CanalChat");
+
+
+	
+}
+
+
+	loadDocA("libros.xml","xml"); /*tenemos un objeto xml */
+
+
+
