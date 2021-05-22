7a835b83a62912d42487665814d5adcbc830a26a
Marcelo <Marcelo.Santonja@iesdoctorbalmis.com>
2021-05-22 16:15:33 +0200

    probando cosas

--- Ejercicio1/Librerias.js
+++ Ejercicio1/Librerias.js
@@ -15,24 +15,26 @@ function gestionarFicheroXML(xmlDoc){
 	console.log(libros[0].childElementCount );
 	console.log(libros[0].children[1]);
 
+	let textoIner = "";
 
-
-	capaVacia.innerHTML += `<div id = "Tabla">`;
+	textoIner += `<div id = "Tabla">`;
 	for (let i = 1; i <= libreria.length; i++) {
-		capaVacia.innerHTML += `<div id= "Libreria">`+nombre[i-1].textContent+`</div><div class="fila"> <div class="celda">ISBN</div> <div class="celda">titulo</div> <div class="celda">Nivel de profundidad</div> <div class="celda">autor</div> <div class="celda">editorial</div> <div class="celda">fecha de publicacion</div> <div class="celda">pagina Web</div ><div class="celda">precio</div></div>`;
+		textoIner += `<div id= "Libreria">`+nombre[i-1].textContent+`</div><div class="fila"> <div class="celda">ISBN</div> <div class="celda">titulo</div> <div class="celda">Nivel de profundidad</div> <div class="celda">autor</div> <div class="celda">editorial</div> <div class="celda">fecha de publicacion</div> <div class="celda">pagina Web</div ><div class="celda">precio</div></div>`;
 
 		for(let j=1; j<=3; j++){
-		capaVacia.innerHTML = capaVacia.innerHTML + `<div class="fila">`;
+		textoIner = textoIner + `<div class="fila">`;
 		
 		for(let k=0; k<libros[(i*j)-1].childElementCount; k++){
-			capaVacia.innerHTML +=	 `<div class="celda">`+libros[(i*j)-1].children[k].textContent+`</div>`;
+			textoIner +=	 `<div class="celda">`+libros[(i*j)-1].children[k].textContent+`</div>`;
 		}
 		
-		capaVacia.innerHTML += `</div>`;
+		textoIner += `</div>`;
 		}
 		
 	}
-	capaVacia.innerHTML += `</div>`;
+	textoIner += `</div>`;
+	capaVacia.innerHTML += textoIner;
+
 
 	
 }
